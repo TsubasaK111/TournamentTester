6d50b225286da64d585328534aeb47b1b7bed3d7
TsubasaK111 <Tsubasa.K111@gmail.com>
2015-11-20 00:34:57 +0900

    fix: deprecated error handling attempt to keep things simple for the time being

--- tournament_run.py
+++ tournament_run.py
@@ -6,14 +6,10 @@ import subprocess
 
 create_database_output = subprocess.check_output("psql -c 'CREATE DATABASE tournament;'", shell=True)
 
-if create_database_output == """ERROR:  database "tournament" already exists""":
-    print("NOTICE: database 'tournament' already exists,"
-          "continuing database setup with current dataset...")
-else:
-    print("create_database_output returned:")
-    print(create_command_output)
+print("create_database_output returned:")
+print(create_database_output)
 
-run_tournament_sql_output = subprocess.check_output("psql -f tournament.sql", shell=True)
+tournament_sql_output = subprocess.check_output("psql -f tournament.sql", shell=True)
 
-print("run_tournament_sql sez:")
-print(run_tournament_sql)
+print("tournament_sql_output returned:")
+print(tournament_sql_output)
