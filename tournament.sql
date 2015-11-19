94d95b7517883e385e33356b72bb1bc6c715ff1a
TsubasaK111 <Tsubasa.K111@gmail.com>
2015-11-20 00:00:20 +0900

    fix: reorder table declaration and add IF NOT EXISTS condition

--- tournament.sql
+++ tournament.sql
@@ -1,28 +1,28 @@
 -- Table definitions for the tournament project.
 
 
---CREATE DATABASE tournament;
+CREATE TABLE IF NOT EXISTS players (
+  player_name text NOT NULL,
+  player_id serial PRIMARY KEY
+);
 
 
-CREATE TABLE matches (
+CREATE TABLE IF NOT EXISTS matches (
   winner_id INTEGER REFERENCES players(player_id),
   loser_id  INTEGER REFERENCES players(player_id),
   match_id  SERIAL PRIMARY KEY
 );
 
 
-CREATE TABLE players (
-  player_name text,
-  player_id serial PRIMARY KEY
-);
-
-
-CREATE ranking AS
+CREATE OR REPLACE VIEW ranking AS
     SELECT winning.player_id,
            winning.player_name,
            winning.wins,
            winning.wins+losing.losses AS number_of_matches,
-           ROW_NUMBER() OVER (ORDER BY winning.wins DESC, losing.losses ASC) AS rank
+           ROW_NUMBER() OVER (
+             ORDER BY winning.wins DESC,
+                      losing.losses ASC
+           ) AS rank
     FROM (
         SELECT player_id, player_name, count(winner_id) AS wins
         FROM players LEFT JOIN matches
